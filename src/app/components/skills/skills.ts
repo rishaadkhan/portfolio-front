40d7fb3265369e4d60833bc3cca284dcd2d0b7c6
Rishaad <rishaad326@gmail.com>
2025-11-30 20:22:02 +0530

    chore(ui): remove project/service images; add PHP, PostgreSQL, Postman to skills

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faJava, faPython, faAngular, faReact, faLaravel, faDocker, faGitAlt } from '@fortawesome/free-brands-svg-icons';
+import { faJava, faPython, faAngular, faReact, faLaravel, faDocker, faGitAlt, faPhp } from '@fortawesome/free-brands-svg-icons';
 import { faDatabase, faCode, faCloud, faGears, faServer } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -16,6 +16,7 @@ export class Skills {
   faJava = faJava;
   faPython = faPython;
   faCode = faCode; // For C#
+  faPhp = faPhp;
 
   // Web & Frameworks Icons
   faAngular = faAngular;
@@ -36,7 +37,8 @@ export class Skills {
     programming: [
       { name: 'Java', icon: this.faJava },
       { name: 'Python', icon: this.faPython },
-      { name: 'C#', icon: this.faCode }
+      { name: 'C#', icon: this.faCode },
+      { name: 'PHP', icon: this.faPhp }
     ],
     webFrameworks: [
       { name: 'Angular', icon: this.faAngular },
@@ -47,12 +49,14 @@ export class Skills {
     databases: [
       { name: 'MySQL', icon: this.faDatabase },
       { name: 'MongoDB', icon: this.faDatabase },
-      { name: 'Firebase', icon: this.faDatabase }
+      { name: 'Firebase', icon: this.faDatabase },
+      { name: 'PostgreSQL', icon: this.faDatabase }
     ],
     tools: [
       { name: 'Azure', icon: this.faCloud },
       { name: 'Docker', icon: this.faDocker },
-      { name: 'Git', icon: this.faGitAlt }
+      { name: 'Git', icon: this.faGitAlt },
+      { name: 'Postman', icon: this.faGears }
     ]
   };
 }
